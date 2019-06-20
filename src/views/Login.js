42403069242072c128027df618314f848701c31a
Fang <git@fang.io>
2019-06-21 01:30:24 +0200

    Make /views/Login functional/stateless

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,8 +1,9 @@
 import { Nothing, Just } from 'folktale/maybe';
-import React from 'react';
+import React, { useState } from 'react';
 import * as azimuth from 'azimuth-js';
 
-import { H1, Row, Col } from '../components/old/Base';
+import { H1 } from 'indigo-react';
+import View from '../components/View';
 import Tabs from '../components/Tabs';
 
 import Ticket from './Login/Ticket';
@@ -10,13 +11,12 @@ import Mnemonic from './Login/Mnemonic';
 import Advanced from './Login/Advanced';
 
 import * as need from '../lib/need';
-import { compose } from '../lib/lib';
 import { ROUTE_NAMES } from '../lib/routeNames';
 
-import { withHistory } from '../store/history';
-import { withNetwork } from '../store/network';
-import { withWallet } from '../store/wallet';
-import { withPointCursor } from '../store/pointCursor';
+import { useHistory } from '../store/history';
+import { useNetwork } from '../store/network';
+import { useWallet } from '../store/wallet';
+import { usePointCursor } from '../store/pointCursor';
 
 const TABS = {
   TICKET: Symbol('TICKET'),
@@ -24,46 +24,39 @@ const TABS = {
   ADVANCED: Symbol('ADVANCED'),
 };
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Login() {
+  // globals
+  const history = useHistory();
+  const { contracts } = useNetwork();
+  const { wallet } = useWallet();
+  const { pointCursor, setPointCursor } = usePointCursor();
 
-    this.state = {
-      currentTab: TABS.TICKET,
-    };
+  // inputs
+  const [currentTab, setCurrentTab] = useState(TABS.TICKET);
 
-    this.handleTabChange = this.handleTabChange.bind(this);
-    this.continue = this.continue.bind(this);
-  }
+  //TODO figure out how to do things once
+  // // we expect wallet and pointCursor to not be set yet
+  // setUrbitWallet(Nothing());
+  // setWallet(Nothing());
+  // setPointCursor(Nothing());
 
-  componentDidMount() {
-    // we expect wallet and pointCursor to not be set yet
-    this.props.setUrbitWallet(Nothing());
-    this.props.setWallet(Nothing());
-    this.props.setPointCursor(Nothing());
-  }
-
-  handleTabChange(currentTab) {
-    this.setState({ currentTab });
-  }
-
-  async continue() {
-    const wallet = need.wallet(this.props);
-    const contracts = need.contracts(this.props);
+  const doContinue = async () => {
+    const realWallet = need.wallet(wallet);
+    const realContracts = need.contracts(contracts);
 
     // if no point cursor set by login logic, try to deduce it
     let deduced = Nothing();
     if (Nothing.hasInstance(this.props.pointCursor)) {
       const owned = await azimuth.azimuth.getOwnedPoints(
-        contracts,
-        wallet.address
+        realContracts,
+        realWallet.address
       );
       if (owned.length === 1) {
         deduced = Just(owned[0]);
       } else if (owned.length === 0) {
         const canOwn = await azimuth.azimuth.getTransferringFor(
-          contracts,
-          wallet.address
+          realContracts,
+          realWallet.address
         );
         if (canOwn.length === 1) {
           deduced = Just(canOwn[0]);
@@ -72,51 +65,40 @@ class Login extends React.Component {
     }
 
     if (Just.hasInstance(deduced)) {
-      this.props.setPointCursor(deduced);
-      this.props.history.popAndPush(ROUTE_NAMES.POINT_HOME);
-    } else if (Just.hasInstance(this.props.pointCursor)) {
-      this.props.history.popAndPush(ROUTE_NAMES.POINT_HOME);
+      setPointCursor(deduced);
+      history.popAndPush(ROUTE_NAMES.POINT_HOME);
+    } else if (Just.hasInstance(pointCursor)) {
+      history.popAndPush(ROUTE_NAMES.POINT_HOME);
     } else {
       //TODO to new overview (maybe should be merged into point home?)
-      this.props.history.popAndPush(ROUTE_NAMES.SHIPS);
+      history.popAndPush(ROUTE_NAMES.POINTS);
     }
-  }
+  };
 
-  render() {
-    const tabOptions = [
-      { title: 'Ticket', value: TABS.TICKET },
-      { title: 'Mnemonic', value: TABS.MNEMONIC },
-      { title: 'Advanced', value: TABS.ADVANCED },
-    ];
+  const tabViews = {
+    [TABS.TICKET]: Ticket,
+    [TABS.MNEMONIC]: Mnemonic,
+    [TABS.ADVANCED]: Advanced,
+  };
 
-    const tabViews = {
-      [TABS.TICKET]: Ticket,
-      [TABS.MNEMONIC]: Mnemonic,
-      [TABS.ADVANCED]: Advanced,
-    };
+  const tabOptions = [
+    { title: 'Ticket', value: TABS.TICKET },
+    { title: 'Mnemonic', value: TABS.MNEMONIC },
+    { title: 'Advanced', value: TABS.ADVANCED },
+  ];
 
-    return (
-      <Row>
-        <Col>
-          <H1>{'Login'}</H1>
+  return (
+    <View>
+      <H1>Login</H1>
 
-          <Tabs
-            tabViews={tabViews}
-            tabOptions={tabOptions}
-            currentTab={this.state.currentTab}
-            onTabChange={this.handleTabChange}
-            //
-            loginCompleted={this.continue}
-          />
-        </Col>
-      </Row>
-    );
-  }
+      <Tabs
+        tabViews={tabViews}
+        tabOptions={tabOptions}
+        currentTab={currentTab}
+        onTabChange={setCurrentTab}
+        //
+        loginCompleted={doContinue}
+      />
+    </View>
+  );
 }
-
-export default compose(
-  withHistory,
-  withNetwork,
-  withWallet,
-  withPointCursor
-)(Login);
