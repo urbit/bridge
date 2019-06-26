0e6fe7a355569c0a83ae5c3431b26fcd1a4a8c95
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-06-26 10:38:54 -0700

    feat: only blink blinky after 2.5s of waiting

--- src/components/Blinky.js
+++ src/components/Blinky.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import useInterval from 'lib/useInterval';
 
 // TODO: make these characters display as the same width
 export const kLoadingCharacter = '▓';
 export const kInterstitialCharacter = '░';
+const kBlinkAfterMs = 2500; // ms
 
 export const matchBlinky = obj =>
   obj.matchWith({
@@ -17,7 +18,14 @@ export default function Blinky({
   b = kInterstitialCharacter,
 }) {
   const [value, setValue] = useState(true);
-  useInterval(() => setValue(val => !val), 1000);
+  const now = useRef(new Date());
+
+  useInterval(() => {
+    // only start blinking if we've elapsed enough time
+    if (new Date() - now.current > kBlinkAfterMs) {
+      setValue(val => !val);
+    }
+  }, 1000);
 
   return value ? a : b;
 }
