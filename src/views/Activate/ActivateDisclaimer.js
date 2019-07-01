225bad97ca4e4c1b2d2cce21cad56141885d20ca
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-07-01 13:08:32 -0700

    feat: switch to f5 for disclaimer text

--- src/views/Activate/ActivateDisclaimer.js
+++ src/views/Activate/ActivateDisclaimer.js
@@ -1,11 +1,14 @@
 import React, { useCallback } from 'react';
-import { Grid, H3, P, B, Flex, Text, CheckboxInput } from 'indigo-react';
+import cn from 'classnames';
+import { Grid, H3, B, Text, CheckboxInput } from 'indigo-react';
 
 import View from 'components/View';
 import { useCheckboxInput } from 'components/Inputs';
 import { ForwardButton } from 'components/Buttons';
 import { useLocalRouter } from 'lib/LocalRouter';
 import WarningBox from 'components/WarningBox';
+
+const kTextStyle = 'f5';
 
 export default function ActivateDisclaimer() {
   const { push, names } = useLocalRouter();
@@ -23,28 +26,28 @@ export default function ActivateDisclaimer() {
           Disclaimer
         </Grid.Item>
         <Grid.Item full>
-          <Text className="f6 block mb4">
+          <Text className={cn(kTextStyle, 'block mb4')}>
             Welcome to Bridge. Please read this information. Your assets could
             be stolen if you do not pay attention to these warnings.
           </Text>
 
-          <Text className="f6 block mb4">
+          <Text className={cn(kTextStyle, 'block mb4')}>
             <B>You own and control your assets. We do not.</B> If you send your
             assets to another address, we can’t get them back for you.
           </Text>
 
-          <B className="f6">Please understand that nobody can:</B>
-          <Text className="f6 block mb4">
+          <B className={kTextStyle}>Please understand that nobody can:</B>
+          <Text className={cn(kTextStyle, 'block mb4')}>
             1. Access your assets for you <br />
             2. Recover, reset, or modify ANY of your information <br />
             3. Reverse, cancel, or refund transactions <br />
             4. Hold, back-up, or access your keys
           </Text>
 
-          <B className="f6">
+          <B className={kTextStyle}>
             You’re responsible for keeping your information safe. This includes:
           </B>
-          <Text className="f6 block mb4">
+          <Text className={cn(kTextStyle, 'block mb4')}>
             1. Private Keys/Mnemonic Phrases and passwords <br />
             2. JSON files <br />
             3. Hardware wallet PINs; <br />
@@ -53,12 +56,12 @@ export default function ActivateDisclaimer() {
             here]
           </Text>
 
-          <B className="f6">To be clear</B>
-          <Text className="f6 block">
+          <B className={kTextStyle}>To be clear</B>
+          <Text className={cn(kTextStyle, 'block')}>
             This software is provided “as is” without warranties of any kind and
             our liability to you in connection with the same is limited;
           </Text>
-          <Text className="f6 block">
+          <Text className={cn(kTextStyle, 'block')}>
             We have no liability for any security problems or incidents that you
             may experience, including any loss or theft of your keys or any
             problems that may arise in connection with your wallet.
