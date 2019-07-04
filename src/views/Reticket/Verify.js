0b0242b08f7b5ee2e71d381ac387262e5b14209b
Fang <git@fang.io>
2019-07-04 11:04:56 +0200

    Use better ticket input verification
    consistent with PassportVerify

--- src/views/Reticket/Verify.js
+++ src/views/Reticket/Verify.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { Input, AccessoryIcon } from 'indigo-react';
 
 import View from 'components/View';
@@ -6,31 +6,35 @@ import { useTicketInput } from 'components/Inputs';
 import { ForwardButton } from 'components/Buttons';
 
 import { useLocalRouter } from 'lib/LocalRouter';
+import { validateExactly } from 'lib/validators';
 
+const STUB_VERIFY_TICKET = process.env.NODE_ENV === 'development';
+
+//TODO deduplicate with PassportVerify
 export default function Verify({ STEP_NAMES, newWallet }) {
   const { push } = useLocalRouter();
 
+  const ticket = newWallet.value.wallet.ticket;
+  const validators = useMemo(
+    () => [validateExactly(ticket, 'Does not match expected master ticket.')],
+    [ticket]
+  );
   const ticketInput = useTicketInput({
     name: 'ticket',
     label: 'New master ticket',
+    initialValue: STUB_VERIFY_TICKET ? ticket : undefined,
+    autoFocus: true,
+    validators,
   });
-  const ticket = ticketInput.data;
+  const { pass } = ticketInput;
 
   const next = useCallback(() => push(STEP_NAMES.RETICKET), [push, STEP_NAMES]);
-
-  const matches = newWallet.value.wallet.ticket === ticket;
-
-  const displayTicketStatus = matches ? (
-    <AccessoryIcon.Success />
-  ) : (
-    <AccessoryIcon.Failure />
-  );
 
   return (
     <View>
       Prove that you downloaded the new secrets!
-      <Input {...ticketInput} accessory={displayTicketStatus} />
-      <ForwardButton disabled={!matches} onClick={next}>
+      <Input {...ticketInput} />
+      <ForwardButton disabled={!pass} onClick={next}>
         Reticket
       </ForwardButton>
     </View>
