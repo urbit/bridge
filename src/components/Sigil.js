be539d4d73aab5666ae430b43d8a1065cb262536
Fang <git@fang.io>
2019-07-25 00:59:16 +0200

    Use latest sigil-js and scale sigils to fill container

--- src/components/Sigil.js
+++ src/components/Sigil.js
@@ -4,6 +4,7 @@ export default function Sigil({ patp, size, colors, ...rest }) {
   return sigil({
     patp,
     renderer: reactRenderer,
+    style: { width: '100%', height: '100%' }, //NOTE scale to container
     size,
     colors,
     ...rest,
