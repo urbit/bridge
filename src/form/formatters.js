d5c7a6f97d206f6523bab31e729f4c71981337f7
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-08-13 09:27:59 +0200

    chore: cleanup/comment ensurePatFormat function

--- src/form/formatters.js
+++ src/form/formatters.js
@@ -1,31 +1,35 @@
 import { compose, strSplice } from 'lib/lib';
 
 const TICKET_MAX_BYTE_LEN = 32; // tickets can be as large as 32 bytes
+const PAT_BLOCK_CHAR_LENGTH = 6; // pat{p,q} blocks are 6 characters long
 
 export const buildFormatter = (formatters = []) =>
   compose(...formatters.reverse());
 
 /**
- * inserts a ~ or - every 7 characters
+ * inserts a ~ or - before every pat block
  */
 export const ensurePatFormat = s => {
+  // bail if falsy or empty string, nothing to do
   if (!s || s.length === 0) {
     return s || '';
   }
 
   // ensure there's a dash every 6 + 1 characters
-  const dashAt = i => i * (6 + 1);
+  const dashAt = i => i * (PAT_BLOCK_CHAR_LENGTH + 1);
   const maxBlocks = TICKET_MAX_BYTE_LEN / 2;
 
+  // for every index that may need a separator
   for (let i = 0; i !== maxBlocks; i++) {
     const dashIndex = dashAt(i);
 
+    // if the string is too short to have a separator here, we're done
     if (s.length - 1 < dashIndex) {
       return s;
     }
 
+    // insert the separator at the correct index if not already there
     const sep = i === 0 ? '~' : '-';
-
     if (s.charAt(dashIndex) !== sep) {
       s = strSplice(s, dashIndex, sep);
     }
