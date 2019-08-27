4b63f96055e2c7aee886e49ef2d0e1af2dbee2b5
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-08-27 16:23:51 +0200

    fix: replace with the world's simplest tooltip

--- src/components/CopiableWithTooltip.js
+++ src/components/CopiableWithTooltip.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import cn from 'classnames';
-import Tooltip from 'react-simple-tooltip';
 import { ReactComponent as Copy } from 'assets/copy.svg';
 
 import useCopiable from 'lib/useCopiable';
+import WithTooltip from 'components/WithTooltip';
 
 export default function CopiableWithTooltip({
   as: As = 'span',
@@ -17,15 +17,13 @@ export default function CopiableWithTooltip({
   return (
     <As className={cn(className, 'nowrap')} {...rest}>
       {children}
-      <Tooltip
-        content={didCopy ? 'Copied!' : 'Copy'}
-        customCss={{ whitespace: 'nowrap' }}>
+      <WithTooltip content={didCopy ? 'Copied!' : 'Copy'}>
         <Copy
           style={{ height: '1em', width: '1em' }}
           className="ml1 pointer"
           onClick={doCopy}
         />
-      </Tooltip>
+      </WithTooltip>
     </As>
   );
 }
