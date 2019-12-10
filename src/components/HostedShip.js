f872505e39eb56023e6ab81f784bac117bf1a62b
Brendan Hay <brendan.g.hay@gmail.com>
2019-12-10 09:37:45 +0100

    Adding links to/from hosting and visualising ship states

--- src/components/HostedShip.js
+++ src/components/HostedShip.js
@@ -1,100 +1,144 @@
 import React from 'react';
-import cn from 'classnames';
 import { Grid, ErrorText } from 'indigo-react';
+
+import cn from 'classnames';
 
 import BridgeForm from 'form/BridgeForm';
 
-import {
-  OutButton,
-  RestartButton,
-  GenerateButton,
-  ForwardButton,
-} from './Buttons';
-import ProgressButton from './ProgressButton';
+import { GenerateButton, ForwardButton } from './Buttons';
+
+import 'style/hosted-ship.scss';
 
 export default function HostedShip({
-  // from useHostedShip.bind
-  error,
-  notFound,
-  running,
-  booting,
-  connecting,
-  connected,
-  unknown,
-  events,
-
-  checkShipExists,
-  getShipEvents,
-  createNewShip,
+  // useHostedShip
+  ship,
 
   // additional from parent
   className,
 }) {
-  const canHost = true;
-
-  const renderPrimarySection = () => {
-    if (notFound) {
-      return (
-        <Grid.Item full as={GenerateButton} onClick={createNewShip} success>
-          Boot Urbit OS
-        </Grid.Item>
-      );
-    } else if (running) {
-      return (
-        <Grid.Item full as={ForwardButton} onClick={getShipEvents} success>
-          Connect
-        </Grid.Item>
-      );
-    } else if (connecting) {
-      return (
-        <Grid.Item full as={ProgressButton} success disabled>
-          Connecting
-        </Grid.Item>
-      );
-    } else if (booting) {
-      return (
-        <Grid.Item full as={ProgressButton} success disabled>
-          Booting
-        </Grid.Item>
-      );
-    } else {
+  const renderStatus = () => {
+    if (ship.missing || ship.booting) {
       return (
         <Grid.Item
           full
-          as={RestartButton}
-          onClick={checkShipExists}
-          disabled={!canHost}
-          loading={!canHost && unknown}>
-          Check Hosting Status
+          as={GenerateButton}
+          solid
+          success
+          disabled={ship.booting}
+          loading={ship.booting}
+          onClick={ship.create}>
+          Boot Urbit OS
+        </Grid.Item>
+      );
+    } else if (ship.running || ship.connecting) {
+      return (
+        <Grid.Item
+          full
+          as={ForwardButton}
+          solid
+          success
+          disabled={ship.connecting}
+          loading={ship.connecting}
+          onClick={ship.getEvents}>
+          Connect
+        </Grid.Item>
+      );
+    } else if (ship.connected) {
+      return (
+        <Grid.Item
+          full
+          as={GenerateButton}
+          solid
+          success
+          onClick={ship.getStatus}>
+          Disconnect
+        </Grid.Item>
+      );
+    } else if (ship.unknown || ship.querying) {
+      return (
+        <Grid.Item
+          full
+          as={GenerateButton}
+          accessory={ship.error ? '↺' : '->'}
+          solid
+          success
+          disabled={ship.querying}
+          loading={ship.querying}
+          onClick={ship.getStatus}>
+          Query
         </Grid.Item>
       );
     }
   };
 
+  const renderEvents = (title, detail, events) => {
+    return (
+      events.length > 0 && (
+        <>
+          <Grid.Item
+            full
+            as={ForwardButton}
+            accessory=""
+            disabled
+            detail={detail}>
+            {title}
+          </Grid.Item>
+          <Grid.Item full>
+            <ul className="hosting-events">
+              {events.map(event => (
+                <li>{event}</li>
+              ))}
+            </ul>
+          </Grid.Item>
+        </>
+      )
+    );
+  };
+
   return (
-    <Grid className={cn(className, 'mt1')}>
-      <BridgeForm>
-        {() => (
-          <>
-            {renderPrimarySection()}
-
-            {error && (
-              <Grid.Item full as={ErrorText} className="mv1">
-                {error.message}
-              </Grid.Item>
-            )}
-
-            {running && (
-              <>
-                <Grid.Item full as={OutButton}>
-                  Redirect to Urbit OS Dojo
+    <>
+      <Grid className={cn(className, 'mt1')}>
+        <BridgeForm>
+          {() => (
+            <>
+              {ship.error && (
+                <Grid.Item full as={ErrorText} className="mv1">
+                  {ship.error.message}
                 </Grid.Item>
-                <Grid.Divider />
-              </>
+              )}
+              {renderStatus()}
+              {!ship.unknown && (
+                <Grid.Item
+                  full
+                  as={ForwardButton}
+                  accessory=""
+                  disabled
+                  detail={ship.state}
+                  detailClassName="mono">
+                  Ship Status
+                </Grid.Item>
+              )}
+            </>
+          )}
+        </BridgeForm>
+
+        {ship.connected && (
+          <>
+            <Grid.Divider />
+            {renderEvents('Run', 'Terminal events', ship.runEvents)}
+            {renderEvents(
+              'New',
+              'Pier creation and boot events',
+              ship.newEvents
+            )}
+            {renderEvents(
+              'Sys',
+              'Hosting platform events',
+              ship.sysEvents.map(e => e.object.message)
             )}
           </>
         )}
-      </BridgeForm>
-    </Grid>
+      </Grid>
+    </>
   );
 }
