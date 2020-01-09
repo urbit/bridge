4e06b3a8dda16f1db451a945770eb81190344a13
Liam Fitzgerald <liam@urizen.co>
2020-01-09 14:37:10 +1100

    add color picker input

--- src/form/Inputs.js
+++ src/form/Inputs.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Input, AccessoryIcon } from 'indigo-react';
+import { Input, AccessoryIcon, Flex } from 'indigo-react';
+import cn from 'classnames';
 import { useField } from 'react-final-form';
 
 import {
@@ -196,3 +197,51 @@ export function EmailInput({ ...rest }) {
     />
   );
 }
+
+export const ColorInput = ({
+  name,
+  colors,
+  disabled = false,
+  label,
+  className,
+}) => {
+  const { input } = useField(name, { subscription: { value: true } });
+
+  const isWhite = c => c === '#FFFFFF';
+
+  // disabled = disabled || submitting || submitSucceeded;
+  return (
+    <Flex col className={cn(className, 'flex-wrap mb1')}>
+      <Flex.Item
+        as="label"
+        className={cn('f6 lh-tall', {
+          black: !disabled,
+          gray4: disabled,
+        })}
+        htmlFor={name}>
+        {label}
+      </Flex.Item>
+      <div className="flex">
+        {colors.map(c => (
+          <button
+            className={cn('m0 p0 h7 w7 p2 flex flex-center', {
+              'b b-black': isWhite(c),
+            })}
+            style={{ backgroundColor: c }}
+            onClick={() => input.onChange(c)}>
+            {c === input.value ? (
+              <div
+                className={cn(
+                  { 'bg-black': isWhite(c), 'bg-white': !isWhite(c) },
+                  'w2 h2 r-full'
+                )}
+              />
+            ) : (
+              <div />
+            )}
+          </button>
+        ))}
+      </div>
+    </Flex>
+  );
+};
