e4b8fc9bc52427ca8b9af557c259d1214318948f
Fang <git@fang.io>
2020-05-05 22:23:23 +0200

    constants: bump planet invite gas limit

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -31,7 +31,7 @@ const GAS_LIMITS = {
   REJECT: 200000, //NOTE no samples
   DETACH: 200000, //NOTE no samples
   //
-  GIFT_PLANET: 420000, //NOTE low sample size, //NOTE also update in gas tank
+  GIFT_PLANET: 450000, //NOTE low sample size, //NOTE also update in gas tank
   //
   SEND_ETH: 21000,
   //
