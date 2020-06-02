21c115c854ecad7907f196f0aeb7fea3eb97a362
Liam Fitzgerald <liam@urizen.co>
2020-06-02 11:39:15 +1000

    fix redraw wiping invite component state

--- src/views/Point.js
+++ src/views/Point.js
@@ -25,6 +25,91 @@ import { useLocalRouter } from 'lib/LocalRouter';
 import Inviter from 'views/Invite/Inviter';
 import { usePointCache } from 'store/pointCache';
 
+function InviteForm({
+  showInviteForm,
+  setShowInviteForm,
+  acceptedInvites,
+  acceptedPoints,
+  availableInvites,
+  goCohort,
+  pendingPoints,
+  sentInvites,
+  showInvites,
+}) {
+  const _totalInvites =
+    sentInvites.getOrElse(0) + availableInvites.getOrElse(0);
+  const _pendingInvites = pendingPoints.getOrElse([]).length;
+
+  return (
+    <>
+      <Grid.Item cols={[1, 11]}>
+        Invite Group
+        <br />
+      </Grid.Item>
+
+      <Grid.Item
+        className={cn('t-right underline pointer-hover', {
+          gray4: sentInvites.getOrElse(0) === 0,
+        })}
+        onClick={goCohort}
+        cols={[11, 13]}>
+        View
+      </Grid.Item>
+      <Grid.Item full>
+        <Flex align="center">
+          <Flex.Item>
+            {acceptedInvites.getOrElse(0)} / {_totalInvites}
+          </Flex.Item>
+          {_pendingInvites > 0 && (
+            <Flex.Item as={Chip} className="bg-yellow1 yellow4">
+              {_pendingInvites} pending
+            </Flex.Item>
+          )}
+        </Flex>
+      </Grid.Item>
+
+      {showInvites && (
+        <>
+          <Grid.Item
+            full
+            as={BarGraph}
+            available={availableInvites}
+            sent={sentInvites}
+            accepted={acceptedInvites}
+          />
+          <Grid.Item
+            full
+            as={InviteSigilList}
+            pendingPoints={pendingPoints}
+            acceptedPoints={acceptedPoints}
+          />
+        </>
+      )}
+      {!showInvites && (
+        <>
+          <Grid.Item full className="b-gray4 b-dotted b1 self-center">
+            <div className="p4 pv8 t-center gray4">
+              Start your invite group by adding members
+            </div>
+          </Grid.Item>
+        </>
+      )}
+      {!showInviteForm && availableInvites.getOrElse(0) > 0 && (
+        <Grid.Item
+          full
+          solid
+          as={Button}
+          center
+          onClick={() => setShowInviteForm(true)}>
+          Add Members
+        </Grid.Item>
+      )}
+      {showInviteForm && <Inviter />}
+      <Grid.Item full className="mb2" />
+    </>
+  );
+}
+
 export default function Point() {
   const { pop, push, names } = useLocalRouter();
   const { pointCursor } = usePointCursor();
@@ -60,10 +145,6 @@ export default function Point() {
   );
 
   const hasInvites = showInvites || availableInvites.getOrElse(0) !== 0;
-
-  const _totalInvites =
-    sentInvites.getOrElse(0) + availableInvites.getOrElse(0);
-  const _pendingInvites = pendingPoints.getOrElse([]).length;
 
   const loadedInvites = Just.hasInstance(availableInvites);
   //
@@ -135,89 +216,6 @@ export default function Point() {
 
   const address = need.addressFromWallet(wallet);
 
-  const InviteForm = useCallback(() => {
-    return (
-      <>
-        <Grid.Item cols={[1, 11]}>
-          Invite Group
-          <br />
-        </Grid.Item>
-
-        <Grid.Item
-          className={cn('t-right underline pointer-hover', {
-            gray4: sentInvites.getOrElse(0) === 0,
-          })}
-          onClick={goCohort}
-          cols={[11, 13]}>
-          View
-        </Grid.Item>
-        <Grid.Item full>
-          <Flex align="center">
-            <Flex.Item>
-              {acceptedInvites.getOrElse(0)} / {_totalInvites}
-            </Flex.Item>
-            {_pendingInvites > 0 && (
-              <Flex.Item as={Chip} className="bg-yellow1 yellow4">
-                {_pendingInvites} pending
-              </Flex.Item>
-            )}
-          </Flex>
-        </Grid.Item>
-
-        {showInvites && (
-          <>
-            <Grid.Item
-              full
-              as={BarGraph}
-              available={availableInvites}
-              sent={sentInvites}
-              accepted={acceptedInvites}
-            />
-            <Grid.Item
-              full
-              as={InviteSigilList}
-              pendingPoints={pendingPoints}
-              acceptedPoints={acceptedPoints}
-            />
-          </>
-        )}
-        {!showInvites && (
-          <>
-            <Grid.Item full className="b-gray4 b-dotted b1 self-center">
-              <div className="p4 pv8 t-center gray4">
-                Start your invite group by adding members
-              </div>
-            </Grid.Item>
-          </>
-        )}
-        {!showInviteForm && availableInvites.getOrElse(0) > 0 && (
-          <Grid.Item
-            full
-            solid
-            as={Button}
-            center
-            onClick={() => setShowInviteForm(true)}>
-            Add Members
-          </Grid.Item>
-        )}
-        {showInviteForm && <Inviter />}
-        <Grid.Item full className="mb2" />
-      </>
-    );
-  }, [
-    showInviteForm,
-    setShowInviteForm,
-    _pendingInvites,
-    _totalInvites,
-    acceptedInvites,
-    acceptedPoints,
-    availableInvites,
-    goCohort,
-    pendingPoints,
-    sentInvites,
-    showInvites,
-  ]);
-
   const _requestCount = requestCount.getOrElse(0);
 
   return (
@@ -247,7 +245,19 @@ export default function Point() {
             )}
           </Grid.Item>
         )}
-        {isPlanet && hasInvites && <InviteForm />}
+        {isPlanet && hasInvites && (
+          <InviteForm
+            showInviteForm={showInviteForm}
+            setShowInviteForm={setShowInviteForm}
+            acceptedInvites={acceptedInvites}
+            acceptedPoints={acceptedPoints}
+            availableInvites={availableInvites}
+            goCohort={goCohort}
+            pendingPoints={pendingPoints}
+            sentInvites={sentInvites}
+            showInvites={showInvites}
+          />
+        )}
         {!loadedInvites && isPlanet && (
           <Grid.Item className="mv2" full>
             Invite Group <Blinky />
