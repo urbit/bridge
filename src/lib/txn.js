d20b79d429ffc18d0f3b6a9d6088b1c130a559f1
Liam Fitzgerald <liam@urizen.co>
2020-06-27 13:24:20 +1000

    txn: always throw an Error on failure
    fixes: #478 #481

--- src/lib/txn.js
+++ src/lib/txn.js
@@ -92,7 +92,7 @@ const signTransaction = async ({
   } else if (walletType === WALLET_TYPES.TREZOR) {
     await trezorSignTransaction(stx, walletHdPath);
   } else if (walletType === WALLET_TYPES.METAMASK) {
-    return metamaskSignTransaction(stx, wallet.address);
+    return metamaskSignTransaction(utx, wallet.address);
   } else {
     stx.sign(wallet.privateKey);
   }
@@ -102,24 +102,16 @@ const signTransaction = async ({
 
 const sendSignedTransaction = (web3, stx, doubtNonceError) => {
   // Handle fake metamask transaction
+  let eventEmitter;
+  let rawTx;
   if (stx instanceof FakeMetamaskTransaction) {
-    const { ethereum } = window;
-    const sendTxs = {
-      method: 'eth_sendTransaction',
-      params: [stx.txnData],
-      from: stx.txnData.from,
-    };
-    return new Promise((resolve, reject) =>
-      ethereum.sendAsync(sendTxs, (err, data) =>
-        err ? reject(err) : resolve(data.result)
-      )
-    );
+    eventEmitter = web3.eth.sendTransaction(stx.txnData);
+  } else {
+    eventEmitter = web3.eth.sendSignedTransaction(rawTx);
+    rawTx = hexify(stx.serialize());
   }
-  const rawTx = hexify(stx.serialize());
-
   return new Promise(async (resolve, reject) => {
-    web3.eth
-      .sendSignedTransaction(rawTx)
+    eventEmitter
       .on('transactionHash', hash => {
         resolve(hash);
       })
@@ -132,7 +124,7 @@ const sendSignedTransaction = (web3, stx, doubtNonceError) => {
         const isNonceError =
           message.includes("the tx doesn't have the correct nonce.") ||
           message.includes('nonce too low');
-        if (isKnownError || (doubtNonceError && isNonceError)) {
+        if (rawTx && (isKnownError || (doubtNonceError && isNonceError))) {
           console.log(
             'tx send error likely from gas tank submission, ignoring:',
             message
@@ -141,7 +133,8 @@ const sendSignedTransaction = (web3, stx, doubtNonceError) => {
           resolve(txHash);
         } else {
           console.error(err);
-          reject(err.message || 'Transaction sending failed!');
+          const wrappedErr = err instanceof Error ? err : new Error(message);
+          reject(wrappedErr);
         }
       });
   });
