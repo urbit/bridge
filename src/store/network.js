6ff95bc60f56359b6df493a15314a7ba11653c12
fang <git@fang.io>
2021-05-25 00:16:57 +0200

    network: get ecliptic & polls addresses from chain
    Instead of hardcoding their contract addresses, use azimuth-js's
    initContractsPartial to retrieve the Ecliptic and Polls contracts
    dynamically.
    
    Depends on as-of-yet unreleased azimuth-js changes (specifically,
    exposing its newContract functions), but going ahead and putting this
    up for review.

--- src/store/network.js
+++ src/store/network.js
@@ -16,10 +16,16 @@ import { NETWORK_TYPES, chainIdToNetworkType } from '../lib/network';
 import { isDevelopment } from '../lib/flags';
 import { BRIDGE_ERROR } from '../lib/error';
 
+import { Grid } from 'indigo-react';
+import View from 'components/View';
+import Blinky from 'components/Blinky';
+
 function _useNetwork(initialNetworkType = null) {
   const [networkType, setNetworkType] = useState(initialNetworkType);
 
   const [metamask, setMetamask] = useState(false);
+  const [web3, setWeb3] = useState(Nothing());
+  const [contracts, setContracts] = useState(Nothing());
 
   useEffect(() => {
     (async () => {
@@ -35,71 +41,90 @@ function _useNetwork(initialNetworkType = null) {
     })();
   }, []);
 
-  const { web3, contracts } = useMemo(() => {
-    // given a web3 provider and contract addresses,
-    // build the web3 and contracts objects
-    const initWeb3 = (provider, contractAddresses) => {
-      // use an in-window eth provider if possible
-      const _provider = metamask ? window.ethereum : provider;
-      const web3 = new Web3(_provider);
-      const contracts = azimuth.initContracts(web3, contractAddresses);
+  useEffect(() => {
+    (async () => {
+      // given a web3 provider and contract addresses,
+      // build the web3 and contracts objects
+      const initWeb3 = async (provider, contractAddresses) => {
+        // use an in-window eth provider if possible
+        const _provider = metamask ? window.ethereum : provider;
+        const web3 = new Web3(_provider);
 
-      return {
-        web3: Just(web3),
-        contracts: Just(contracts),
+        let contracts = await azimuth.initContractsPartial(
+          web3,
+          contractAddresses.azimuth
+        );
+        contracts.linearSR = azimuth.newLinearStarRelease(
+          web3,
+          contractAddresses.linearSR
+        );
+        contracts.conditionalSR = azimuth.newConditionalStarRelease(
+          web3,
+          contractAddresses.conditionalSR
+        );
+        contracts.delegatedSending = azimuth.newDelegatedSending(
+          web3,
+          contractAddresses.delegatedSending
+        );
+
+        setWeb3(Just(web3));
+        setContracts(Just(contracts));
+        return;
       };
-    };
 
-    switch (networkType) {
-      case NETWORK_TYPES.LOCAL: {
-        const protocol = isDevelopment ? 'ws' : 'wss';
-        const endpoint = `${protocol}://${document.location.hostname}:8545`;
+      switch (networkType) {
+        case NETWORK_TYPES.LOCAL: {
+          const protocol = isDevelopment ? 'ws' : 'wss';
+          const endpoint = `${protocol}://${document.location.hostname}:8545`;
 
-        return initWeb3(
-          new Web3.providers.WebsocketProvider(endpoint),
-          CONTRACT_ADDRESSES.DEV
-        );
-      }
-      case NETWORK_TYPES.ROPSTEN: {
-        const endpoint = `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_ENDPOINT}`;
+          initWeb3(
+            new Web3.providers.WebsocketProvider(endpoint),
+            CONTRACT_ADDRESSES.DEV
+          );
+          return;
+        }
+        case NETWORK_TYPES.ROPSTEN: {
+          const endpoint = `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_ENDPOINT}`;
 
-        return initWeb3(
-          new Web3.providers.HttpProvider(endpoint),
-          CONTRACT_ADDRESSES.ROPSTEN
-        );
-      }
-      case NETWORK_TYPES.MAINNET: {
-        const endpoint = `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ENDPOINT}`;
+          initWeb3(
+            new Web3.providers.HttpProvider(endpoint),
+            CONTRACT_ADDRESSES.ROPSTEN
+          );
+          return;
+        }
+        case NETWORK_TYPES.MAINNET: {
+          const endpoint = `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ENDPOINT}`;
 
-        return initWeb3(
-          new Web3.providers.HttpProvider(endpoint),
-          CONTRACT_ADDRESSES.MAINNET
-        );
-      }
-      case NETWORK_TYPES.OFFLINE:
-      default: {
-        // NB (jtobin):
-        //
-        // The 'offline' network type targets the mainnet contracts, but does not
-        // actually use a provider to connect.  We use a web3 instance to
-        // initalise the contracts, but the network itself is set to Nothing.
-        //
-        // We may want to offer the ability to select a target network for
-        // transactions when offline.
+          initWeb3(
+            new Web3.providers.HttpProvider(endpoint),
+            CONTRACT_ADDRESSES.MAINNET
+          );
+          return;
+        }
+        case NETWORK_TYPES.OFFLINE:
+        default: {
+          // NB (jtobin):
+          //
+          // The 'offline' network type targets the mainnet contracts, but does
+          // not actually use a provider to connect.  We use a web3 instance to
+          // initalise the contracts, but the network itself is set to Nothing.
+          //
+          // We may want to offer the ability to select a target network for
+          // transactions when offline.
 
-        // Note: example.com:3456 doesn't actually point to anything, we just need
-        // a provider to initialize the Web3 object
-        return {
-          ...initWeb3(
+          // Note: example.com:3456 doesn't actually point to anything, we just
+          // need a provider to initialize the Web3 object
+          initWeb3(
             new Web3.providers.HttpProvider('http://example.com:3456'),
             isDevelopment ? CONTRACT_ADDRESSES.DEV : CONTRACT_ADDRESSES.MAINNET
-          ),
-          web3: Nothing(),
+          );
+          setWeb3(Nothing());
           // ^ overwrite the web3 object from initWeb3 with a Nothing
           // to indicate that there is no valid web3 connection
-        };
+          return;
+        }
       }
-    }
+    })();
   }, [networkType, metamask]);
 
   return { networkType, setNetworkType, web3, contracts };
@@ -110,9 +135,24 @@ const NetworkContext = createContext(null);
 // provider
 export function NetworkProvider({ initialNetworkType, children }) {
   const value = _useNetwork(initialNetworkType);
-  return (
-    <NetworkContext.Provider value={value}>{children}</NetworkContext.Provider>
-  );
+
+  if (Just.hasInstance(value.contracts)) {
+    return (
+      <NetworkContext.Provider value={value}>
+        {children}
+      </NetworkContext.Provider>
+    );
+  } else {
+    return (
+      <View inset>
+        <Grid>
+          <Grid.Item full className="mt8 t-center">
+            <Blinky /> Connecting...
+          </Grid.Item>
+        </Grid>
+      </View>
+    );
+  }
 }
 
 // hook consumer
