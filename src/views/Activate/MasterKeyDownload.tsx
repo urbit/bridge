a3feb624bdc72d8d5f2c58f6c79c0a71ca633e15
tomholford <tomholford@users.noreply.github.com>
2021-09-09 15:54:36 -0700

    ux: Master Key Download

--- src/views/Activate/MasterKeyDownload.tsx
+++ src/views/Activate/MasterKeyDownload.tsx
@@ -1,4 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Just, Nothing } from 'folktale/maybe';
+import * as need from 'lib/need';
+import * as ob from 'urbit-ob';
 
 import { ActivateSteps } from './ActivateSteps';
 import { Box } from '@tlon/indigo-react';
@@ -10,10 +13,54 @@ import { FadeableMasterKeyCopy as MasterKeyCopy } from './MasterKeyCopy';
 import { FadeableMasterKeyPresenter as MasterKeyPresenter } from './MasterKeyPresenter';
 import { useActivateFlow } from './ActivateFlow';
 import ActivateView from './ActivateView';
+import { useLocalRouter } from 'lib/LocalRouter';
+import { compileNetworkingKey } from 'lib/keys';
+import { downloadWallet } from 'lib/invite';
+import PaperBuilder from 'components/PaperBuilder';
 
 const MasterKeyDownload = () => {
-  const { setIsIn } = useActivateFlow();
+  const {
+    derivedPoint,
+    derivedWallet,
+    generated,
+    setGenerated,
+    setIsIn,
+  } = useActivateFlow();
+  const { push, names } = useLocalRouter();
+  const point = need.point(derivedPoint);
+  const wallet = need.wallet(derivedWallet);
+  const ticket = wallet.ticket;
+
+  const [paper, setPaper] = useState(Nothing());
   const [triggerAnimation, setTriggerAnimation] = useState<boolean>(false);
+  const [downloaded, setDownloaded] = useState<boolean>(false);
+
+  const pointAsString = derivedPoint.matchWith({
+    Nothing: () => '',
+    Just: p => p.value.toFixed(),
+  });
+
+  const download = useCallback(() => {
+    const netkey = compileNetworkingKey(wallet.network.keys, point, 1);
+    //TODO  could be deduplicated with useKeyfileGenerator's logic
+    const filename = ob.patp(point).slice(1) + '-1.key';
+    downloadWallet(paper.getOrElse([]), netkey, filename);
+    setDownloaded(true);
+  }, [paper, wallet, point, setDownloaded]);
+
+  // sync paper value to activation state
+  useEffect(
+    () =>
+      setGenerated(
+        paper.matchWith({
+          Nothing: () => false,
+          Just: () => true,
+        })
+      ),
+    [paper, setGenerated]
+  );
+
+  const goToVerify = useCallback(() => push(names.VERIFY), [push, names]);
 
   const header = useMemo(() => {
     return triggerAnimation ? (
@@ -30,11 +77,14 @@ const MasterKeyDownload = () => {
 
   const footer = useMemo(() => {
     return triggerAnimation ? (
-      <ActivateButton onClick={() => console.log('download')}>
-        Download Backup
+      <ActivateButton
+        disabled={!generated}
+        onClick={!downloaded ? download : goToVerify}
+        success={downloaded}>
+        {!downloaded ? 'Download Backup (Passport)' : 'Continue'}
       </ActivateButton>
     ) : null;
-  }, [triggerAnimation]);
+  }, [download, downloaded, generated, goToVerify, triggerAnimation]);
 
   const delayedReveal = useCallback(async () => {
     setTimeout(() => {
@@ -64,11 +114,18 @@ const MasterKeyDownload = () => {
           {triggerAnimation && (
             <DangerBox>Do not share this with anyone else!</DangerBox>
           )}
-          <MasterKeyPresenter fadeIn={true} />
-          {triggerAnimation && <MasterKeyCopy text={'test'} />}
+          {ticket && <MasterKeyPresenter fadeIn={true} ticket={ticket} />}
+          {triggerAnimation && <MasterKeyCopy text={ticket} />}
         </Box>
       </ActivateView>
       <ActivateSteps currentStep={1} totalSteps={4} />
+      <PaperBuilder
+        point={pointAsString}
+        wallets={[wallet]}
+        callback={data => {
+          setPaper(Just(data));
+        }}
+      />
     </>
   );
 };
