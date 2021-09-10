0a0562dce139146152c99cb6fb576b25b1f6031b
tomholford <tomholford@users.noreply.github.com>
2021-09-10 07:58:55 -0700

    ux: Confirm Master Key
    - tweak ActivateView layout to support multiple views
    - add helpers to formatters , validators

--- src/views/Activate/ActivateButton.tsx
+++ src/views/Activate/ActivateButton.tsx
@@ -5,33 +5,28 @@ import withFadeable from './withFadeable';
 interface ActivateButtonProps {
   children: React.ReactNode | string;
   onClick: MouseEventHandler;
-  success?: boolean;
+  disabled?: boolean;
 }
 
 const ActivateButton = ({
   children,
   onClick,
-  success,
+  disabled,
+  ...rest
 }: ActivateButtonProps & React.ComponentPropsWithoutRef<'button'>) => {
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      flexWrap="nowrap"
-      height={'100%'}
-      justifyContent="flex-end">
-      <Button
-        onClick={onClick}
-        backgroundColor={success ? 'rgb(70,156,106)' : 'black'}
-        color={'white'}
-        padding={'16px'}
-        fontFamily="Inter"
-        height={'50px'}
-        fontWeight={'400'}
-        fontSize={'18px'}>
-        {children}
-      </Button>
-    </Box>
+    <Button
+      onClick={onClick}
+      backgroundColor={disabled ? 'rgba(0,0,0,0.2)' : 'black'}
+      color={'white'}
+      padding={'16px'}
+      fontFamily="Inter"
+      height={'50px'}
+      fontWeight={'400'}
+      fontSize={'18px'}
+      {...rest}>
+      {children}
+    </Button>
   );
 };
 
