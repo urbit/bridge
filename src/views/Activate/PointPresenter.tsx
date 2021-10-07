1541f1c1a829c607ee17b2fea2785cd2a4c68a62
tomholford <tomholford@users.noreply.github.com>
2021-10-07 01:50:37 -0700

    ui: show animation on Transfer view
    The sigil now has a breathing green aura on success

--- src/views/Activate/PointPresenter.tsx
+++ src/views/Activate/PointPresenter.tsx
@@ -1,4 +1,5 @@
 import { Box, Text } from '@tlon/indigo-react';
+import cn from 'classnames';
 import Sigil from 'components/Sigil';
 import withFadeable from './withFadeable';
 
@@ -25,23 +26,25 @@ const PointPresenter = ({
       justifyContent="center"
       alignItems="center"
       background="white"
-      className={className}>
-      <Box
-        background={success ? '#009F65' : 'black'}
-        borderRadius={5}
-        padding={24}
-        mb={3}
-        width={64}
-        height={64}>
-        <Sigil
-          patp={patp}
-          colors={[success ? '#009F65' : 'black', 'white']}
-          size={64}
-          display="block"
-          padding={32}
+      className={cn(['point-presenter', className])}>
+      <Box className={cn(['pulsating-background', success && 'enter'])} mb={3}>
+        <Box
+          background={success ? '#009F65' : 'black'}
           borderRadius={5}
-        />
+          padding={24}
+          width={64}
+          height={64}>
+          <Sigil
+            patp={patp}
+            colors={[success ? '#009F65' : 'black', 'white']}
+            size={64}
+            display="block"
+            padding={32}
+            borderRadius={5}
+          />
+        </Box>
       </Box>
+
       {showLabel && (
         <Box>
           <Text
