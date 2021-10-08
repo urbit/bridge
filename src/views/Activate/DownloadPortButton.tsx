ae01be41782bec57b38b37dde72d003def910afb
tomholford <tomholford@users.noreply.github.com>
2021-10-07 18:52:18 -0700

    ux: link to new Port for Windows
    Yesterday Port added native support, so this points the download button
    to the Port latest downloads page
    for Windows users.
    
    Also, extract the button into its own component

---
+++ src/views/Activate/DownloadPortButton.tsx
@@ -0,0 +1,30 @@
+import { useMemo } from 'react';
+import { isMacOs, isWindows } from 'react-device-detect';
+import ActivateButton from './ActivateButton';
+
+export const DownloadPortButton = ({ error }) => {
+  const downloadButtonLabel = useMemo(() => {
+    return isMacOs
+      ? 'Download the Client for Mac'
+      : isWindows
+      ? 'Download the Client for Windows'
+      : 'Download the Client for Your OS';
+  }, []);
+
+  const downloadUrl = useMemo(() => {
+    return isMacOs
+      ? `https://github.com/urbit/port/releases/latest/download/Port.dmg`
+      : 'https://github.com/urbit/port/releases/latest';
+  }, []);
+
+  return (
+    <>
+      <ActivateButton
+        onClick={() => window.open(downloadUrl)}
+        disabled={error}
+        success={true}>
+        {downloadButtonLabel}
+      </ActivateButton>
+    </>
+  );
+};
