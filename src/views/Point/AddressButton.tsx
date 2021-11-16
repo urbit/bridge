37686197bfdb3f6452bbae27f2bb7395dc683037
tomholford <tomholford@users.noreply.github.com>
2021-11-15 17:28:29 -0800

    star-ops-ux: Spawn view enhancements
    - refactor using Indigo components
    - shuffle suggested point names
    - click point to populate name field
    - click Address button to populate ownership field

---
+++ src/views/Point/AddressButton.tsx
@@ -0,0 +1,29 @@
+import * as need from 'lib/need';
+import { Button, ButtonProps } from '@tlon/indigo-react';
+import { useCallback } from 'react';
+import { useForm } from 'react-final-form';
+import { useWallet } from 'store/wallet';
+
+interface AddressButtonProps {
+  children?: React.ReactNode;
+  rest?: ButtonProps;
+}
+
+export const AddressButton = ({ children, ...rest }: AddressButtonProps) => {
+  const { change } = useForm();
+  const { wallet }: any = useWallet();
+  const address = need.addressFromWallet(wallet);
+
+  const onClick = useCallback(
+    (_event: React.MouseEvent<HTMLSpanElement>) => {
+      change('owner', address);
+    },
+    [address, change]
+  );
+
+  return (
+    <Button onClick={onClick} {...rest} className={'secondary'}>
+      {children}
+    </Button>
+  );
+};
---
+++ src/views/Point/PatpBadge.tsx
@@ -0,0 +1,27 @@
+import { Badge, TextProps } from '@tlon/indigo-react';
+import { useCallback } from 'react';
+import { useForm } from 'react-final-form';
+
+interface PatpBadgeProps {
+  children?: React.ReactNode;
+  className?: string;
+  rest?: TextProps;
+}
+
+export const PatpBadge = ({ children, className, ...rest }: PatpBadgeProps) => {
+  const { change } = useForm();
+
+  const onClick = useCallback(
+    (event: React.MouseEvent<HTMLSpanElement>) => {
+      const patp = event.currentTarget.innerText;
+      change('point', patp);
+    },
+    [change]
+  );
+
+  return (
+    <Badge onClick={onClick} {...rest} className={className}>
+      {children}
+    </Badge>
+  );
+};
