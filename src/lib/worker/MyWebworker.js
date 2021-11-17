b030c98ba3e4d14ea68759dc9e1d42728ef66537
Will Galebach <willbach@gmail.com>
2021-11-16 21:19:34 -0500

    Linting fixes

--- src/lib/worker/MyWebworker.js
+++
@@ -1,8 +0,0 @@
-export default function MyWorker(args) {
-  console.log('ARGS', args)
-  let onmessage = e => { // eslint-disable-line no-unused-vars
-      // Write your code here...
-      console.log('MESSAGE', e)
-      postMessage("Response");
-  };
-}
