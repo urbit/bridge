2305dbdf4df1c89f5a267c0795b47ef5740d5712
tomholford <tomholford@users.noreply.github.com>
2022-10-13 10:47:20 -0700

    devex: monkeypatches galores

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {Buffer} from "buffer";
+window.Buffer = Buffer;
 
+import ReactDOM from 'react-dom';
 import Bridge from './Bridge';
 
-// if (process.env.NODE_ENV !== 'production') {
-//   require('@welldone-software/why-did-you-render')(React);
-// }
 
 ReactDOM.render(<Bridge />, document.getElementById('root'));
--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,53 @@
 import { defineConfig } from 'vite'
+import GlobalPolyFill from "@esbuild-plugins/node-globals-polyfill";
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
 import { comlink } from 'vite-plugin-comlink'
 import { resolve } from 'path';
-import nodePolyfills from 'vite-plugin-node-stdlib-browser';
-import commonjs from 'vite-plugin-commonjs'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
+    https: true,
   },
-  plugins: [comlink(),  commonjs({ dynamic: { loose: false }}), svgr(), react(), nodePolyfills(),],
+  plugins: [basicSsl(), comlink(), svgr(), react(),],
   build: {
     outDir: 'build',
   },
+  optimizeDeps: {
+    esbuildOptions: {
+        define: {
+            global: "globalThis",
+        },
+        plugins: [
+            GlobalPolyFill({
+                process: true,
+                buffer: true,
+            }),
+        ],
+    },
+  },
   resolve: {
-    alias: 
-    [
-      'assets',
-      'components',
-      'form',
-      'indigo-react',
-      'lib',
-      'store',
-      'style',
-      'views',
-      'worker',
-    ].map(a => ({ find: a, replacement: resolve(__dirname, `src/${a}`)}))
+    alias: [
+      { find: 'buffer', replacement: 'buffer' },      
+      { find: 'process', replacement: 'process/browser' },      
+      { find: 'stream', replacement: 'stream-browserify' },      
+      { find: 'https', replacement: 'agent-base' },      
+      ...[
+        'assets',
+        'components',
+        'form',
+        'indigo-react',
+        'lib',
+        'store',
+        'style',
+        'views',
+        'worker',
+      ].map(a => ({ find: a, replacement: resolve(__dirname, `src/${a}`)})),
+    ],
+
   },
   worker: {
     plugins: [
