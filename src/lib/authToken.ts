08b1cbf4507879611fde84fd23b169e526a05e01
tomholford <tomholford@users.noreply.github.com>
2022-10-18 20:29:44 -0700

    deps: upgrade @ethereumjs/utils
    also:
    - simplify signMessage
    - add note on why tokens generated with MM vs seed phrase do not tie out

--- src/lib/authToken.ts
+++ src/lib/authToken.ts
@@ -3,11 +3,10 @@ import { ecdsaSign } from 'secp256k1';
 import Web3 from 'web3';
 import { hexToBytes } from 'web3-utils';
 import WalletConnect from '@walletconnect/client';
-
+import { hashPersonalMessage } from '@ethereumjs/util';
 import { WALLET_TYPES } from './constants';
 import BridgeWallet from './types/BridgeWallet';
 import { Hash } from '@urbit/roller-api';
-import { keccak256 } from 'ethereumjs-util';
 
 const MESSAGE = 'Bridge Authentication Token';
 
@@ -15,12 +14,22 @@ function signMessage(privateKey: Buffer, useLegacyTokenSigning = false) {
   const msg = '\x19Ethereum Signed Message:\n' + MESSAGE.length + MESSAGE;
   const hashed = useLegacyTokenSigning
     ? crypto.sha256(Buffer.from(msg))
-    : keccak256(Buffer.from(msg));
-  const { signature } = ecdsaSign(Buffer.from(hashed), privateKey);
+    : hashPersonalMessage(Buffer.from(MESSAGE)) // msg prefix is handled by lib
+
+  const hashAry = new Uint8Array(hashed.buffer);
+  const { signature, recid: _recid } = ecdsaSign(hashAry, privateKey);
 
   // add key recovery parameter
   const ethSignature = new Uint8Array(65);
   ethSignature.set(signature);
+
+  // https://eips.ethereum.org/EIPS/eip-155
+  // This method for setting the recovery parameter is the "legacy" way of doing
+  // so; and in fact, does not match up with output of Metamask / Brave Wallet.
+  // MM uses 28 instead of 27.
+  // TODO: Should we bring this up to latest standard? The problem is, users
+  // who have set network keys or generated invites with an older token will
+  // not be able to rederive them
   const v = (ethSignature[32] & 1) + 27;
   ethSignature[64] = v;
 
