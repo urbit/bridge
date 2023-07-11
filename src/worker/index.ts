466bf89be2ea8a64dc9d6549b517eae7e927a3ea
dillon <dpett122@gmail.com>
2023-07-11 19:00:32 -0400

    rm unnecessary comments

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,7 +1,6 @@
 import { Buffer } from 'buffer/';
-// eslint-disable-next-line
 (self as any).Buffer = Buffer;
 
 export const walletgenWorker = new ComlinkWorker<typeof import('./worker')>(
   new URL('./worker.ts', import.meta.url)
-);
+)
