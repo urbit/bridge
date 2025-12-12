920919e2ed45f1d45cdc4b065deb691a8646a2f5
~sarlev-sarsen <tlatfcp@protonmail.com>
2025-12-11 17:23:01 -0800

    update greeting with ways to run urbit os, fixes #1031 and #391

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,6 +3,15 @@ import cn from 'classnames';
 import ob from 'urbit-ob';
 import { Grid, Text, LinkButton } from 'indigo-react';
 
+import {
+  Box,
+  Button,
+  Checkbox,
+  Icon,
+  RadioButton,
+  Row,
+} from '@tlon/indigo-react';
+
 import useWasGreeted from 'lib/useWasGreeted';
 
 const TEXT_STYLE = 'f5';
@@ -26,27 +35,70 @@ export default function ActivateDisclaimer({ point }) {
         <Text className={cn(TEXT_STYLE, 'block mb4')}>
           Welcome <span className="mono">{pointName}</span>,
         </Text>
-
         <Text className={cn(TEXT_STYLE, 'block mb2')}>
-          As of this moment, you own a piece of Urbit. No one can take it from
-          you, and you can keep it for the rest of your life.
+          As of this moment, you own a piece of Urbit.
+          Keep your login details safe. No one can recover them for you.
         </Text>
-        <Text className={cn(TEXT_STYLE, 'block mb2')}>
-          Keep your Master Ticket safe. No one can recover it for you. But it
-          can get you back into Urbit at any time.
-        </Text>
-        <Text className={cn(TEXT_STYLE, 'block mb2')}>Right now you can:</Text>
+        <Text className={cn(TEXT_STYLE, 'block mb2')}>Now that you own your identity, the next step is to run your own operating system:</Text>
       </Grid.Item>
 
-      <Grid.Item full as={LinkButton} href="https://docs.urbit.org/get-on-urbit#get-the-urbit-runtime">
-        <Text className={cn(TEXT_STYLE, 'block mb2')}>
-          Boot Arvo, the Urbit OS
-        </Text>
+      <Grid.Item full>
+        <Box className="info-list">
+          <Row className="info-row">
+            <Box className="info-message">
+              Comfortable with a command line?
+              <br></br>
+              <a
+                style={{ fontWeight: 600 }}
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://docs.urbit.org/get-on-urbit#get-the-urbit-runtime">
+                Run it using Vere↗
+              </a>
+            </Box>
+          </Row>
+          <Row className="info-row">
+            <Box className="info-message">
+              Graphical interfaces more your speed?
+
+              <br></br>
+              <a
+                style={{ fontWeight: 600 }}
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://nativeplanet.io/software">
+                Check out Groundseg↗
+              </a>
+            </Box>
+          </Row>
+          <Row className="info-row">
+            <Box className="info-message">
+              Want a dedicated Urbit device to do it all for you?
+
+              <br></br>
+              <a
+                style={{ fontWeight: 600 }}
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://nativeplanet.io/hardware">
+                Get Native Planet hardware↗
+              </a>
+            </Box>
+          </Row>
+          <Row className="info-row">
+            <Box style={{ fontStyle: 'italic' }} className="info-message">
+              Not ready to self-host? Contact <a className="bold underline" href="mailto:support@tlon.io">support@tlon.io</a> to have them host your Urbit for you!
+            </Box>
+          </Row>
+        </Box>
       </Grid.Item>
 
       <Grid.Item full>
         <Text className={cn(TEXT_STYLE, 'block mb4')}>
-          Welcome to Urbit. See you online.
+          Welcome to Urbit. See you on the network.
         </Text>
       </Grid.Item>
       <Grid.Item full as={LinkButton} onClick={dismiss}>
